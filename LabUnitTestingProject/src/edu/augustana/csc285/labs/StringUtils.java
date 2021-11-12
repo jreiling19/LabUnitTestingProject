d167fa3124b1358d26e4446218e2b35107fd5b51
jreil <jreil@172.16.99.244>
2021-11-12 07:59:15 -0600

    Changed StringUtil.java to avoid duplication

--- LabUnitTestingProject/src/edu/augustana/csc285/labs/StringUtils.java
+++ LabUnitTestingProject/src/edu/augustana/csc285/labs/StringUtils.java
@@ -42,45 +42,9 @@ public class StringUtils {
 		return output.toString(); 
 	}
 	
-	public static void main(String[] args) {
-
-=======
-	 * If the shift key is 2, EMU -> GOW 
-	 * Also, if the key is -2, GOW -> EMU (each letter is replaced by the letter two before it.)
-	 * (Notice that a message can *always* be decoded by using the negative of the original key.)
-	 * 
-	 * If a letter goes past the beginning/end of the alphabet, it should wrap around.
-	 * e.g., if the shift key is 1, ZOOM -> APPN
-	 *  
-	 * Spaces ' ' in the message should be copied as-is into the encoded message.  
-	 * 
-	 * @param message - the text to be encoded (precondition: we assume message contains only upper case letters and spaces).   
-	 * @param keyShiftAmount - the number of letters to shift each character 
-	 * 
-	 * @return the encoded text 
-	 */
-	public static String caesarCipher(String message, int keyShiftAmount) {
-		StringBuilder output = new StringBuilder();
-
-		for (int i = 0; i < message.length(); i++) {
-			char ch = message.charAt(i);
-			if (ch == ' ') {
-				output.append(" ");
-			} else // it's an upper case letter of the alphabet
-			{
-				char newCh = (char) (ch + keyShiftAmount);
-				if (newCh > 'Z') {
-					newCh = (char) (newCh - 26);
-				}
-				output.append(newCh);
-			}
-		}
-		return output.toString(); 
-	}
 	
 	public static void main(String[] args) {
-		System.out.println(StringUtils.caesarCipher("XYZ", 1));
->>>>>>> branch 'main' of https://github.com/jreiling19/LabUnitTestingProject.git
+
 	}
 
 }
